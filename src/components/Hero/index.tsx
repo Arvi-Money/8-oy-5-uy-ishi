140d77b60982602d7f43882b997ad5dc8ee06ba7
sakiynam <mamatisayevasarvinoz@gmail.com>
2024-04-18 22:53:00 +0500

    commit

---
+++ src/components/Hero/index.tsx
@@ -0,0 +1,19 @@
+import Card from '../Card'
+import '../index.css'
+
+const Hero = () => {
+  return (
+      <div className="hero-wrapper">
+          <div className="container">
+              <h1>Xe Currency Converter</h1>
+              <h4>Check live foreign currency exchange rates</h4>
+             
+                  <Card></Card>
+              
+          </div>
+
+    </div>
+  )
+}
+
+export default Hero
